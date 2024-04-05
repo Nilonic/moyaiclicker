0939c12728535918787165c8cd0408e31b547eba
Nilonic <aaaaaa1243999999@gmail.com>
2024-04-05 16:17:09 +0100

    stuff

--- gitSync.sh
+++ gitSync.sh
@@ -1,4 +1,5 @@
 git pull # pull first
+git add * # in case of untracked changes
 git stage * # stage everything
 git commit # as we don't specify -m, it'll ask for the message
 git push # finally, push
